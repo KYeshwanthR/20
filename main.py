435782295b62ed8406664e817f69a79c5b35d581
YeshwanthRam <yeswanthram.kamavarapu@gmail.com>
2024-11-20 19:36:49 +0530

    fixed render

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def displayPDF(file):
     with open(file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}`" width="100%" height="1000" type="application/pdf"></iframe>'
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="1000" type="application/pdf"></iframe>'
 
     st.markdown(pdf_display, unsafe_allow_html=True)
 
