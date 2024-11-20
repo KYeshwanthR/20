8d34f3d7ef30da63f9d67a773bcc836a1412cfa5
YeshwanthRam <yeswanthram.kamavarapu@gmail.com>
2024-11-20 19:49:32 +0530

    updated render

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import streamlit as st
-import base64 
+import streamlit as st 
+from streamlit_pdf_viewer import pdf_viewer
 
-st.set_page_config(layout='wide')
 
-def displayPDF(file):
-    with open(file, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="1000" type="application/pdf"></iframe>'
-
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
-displayPDF('./MsBits.pdf')
+pdf_viewer('MsBits.pdf',render_text=True)
