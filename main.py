6f829fb3733b2d80e927042aac1ab71037aa1e97
YeshwanthRam <yeswanthram.kamavarapu@gmail.com>
2024-11-20 19:30:49 +0530

    pdf display

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def displayPDF(file):
     with open(file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}#view=TwoPage" width="100%" height="1000" type="application/pdf"></iframe>'
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}`" width="100%" height="1000" type="application/pdf"></iframe>'
 
     st.markdown(pdf_display, unsafe_allow_html=True)
+
+displayPDF('MsBits.pdf')
